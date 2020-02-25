89e0fe7b994139e68fbcfadc9abe2161f5884fd5
github username <github email address>
2020-02-25 15:56:47 -0700

    create Customer accounts method

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,5 +13,32 @@ class Customer
         @@all
     end
 
+    def accounts
+        Account.all.select do |account|
+            account.customer == self
+            # binding.pry
+        end
+    end
+
+    def banks
+        accounts.map do |account|
+            account.bank
+        end
+    end
+
+    def new_account(number, balance, bank)
+        Account.new(number, balance, bank, self)
+        
+        #  binding.pry
+        
+    end
+
+    def self.average_number_of_accounts
+        (Account.all.size / all.size.to_f).round(2)
+        # binding.pry
+
+    end
+
+    
     
 end
