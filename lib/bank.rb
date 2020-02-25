d45cf6b00b1c43abe5045ada1b5a7a851aa67cc3
github username <github email address>
2020-02-25 15:56:36 -0700

    create Bank class

--- lib/bank.rb
+++ lib/bank.rb
@@ -12,4 +12,36 @@ class Bank
     def self.all
         @@all
     end
+
+    def accounts
+        Account.all.select do |account|
+            account.bank == self 
+        end
+    end
+
+    def customers
+        accounts.map do |account|
+            account.customer
+        end
+    end
+
+    def self.most_customers
+        bank = all.max_by do |bank|
+            bank.customers.size
+        end
+        p "#{bank.name} Bank has #{bank.accounts.size} customers."
+    end
+
+    def new_account(number, balance, customer)
+        Account.new(number, balance, self, customer)
+    end
+
+    def oldest_account
+        accounts.min_by {|account| account.open_year}
+    end
+
+    def self.customer_count
+        
+    end
+
 end 
